292624ce333e330195c537e4c09239e3906dbf74
skogsgren <6107181+skogsgren@users.noreply.github.com>
2023-11-08 09:36:51 +0100

    remove mpv scripts

--- mpv/scripts/audio-osc.lua
+++
@@ -1,7 +0,0 @@
--- https://old.reddit.com/r/mpv/comments/n2esl7/how_to_always_show_progress_bar_when_playing/gwj6y0l/
-mp.register_event("file-loaded", function()
-    local hasvid = mp.get_property_osd("video") ~= "no"
-    mp.commandv("script-message", "osc-visibility", (hasvid and "auto" or "always"), "no-osd")
-    -- remove the next line if you don't want to affect the osd-bar config
-    mp.commandv("set", "options/osd-bar", (hasvid and "yes" or "no"))
-end)
