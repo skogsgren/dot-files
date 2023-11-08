ef1f7a1e4c1d09877a34c5dc2beb6291990b7d00
skogsgren <6107181+skogsgren@users.noreply.github.com>
2023-11-08 09:39:17 +0100

    switch to shutil since distutils is becoming deprecated

--- stow.py
+++ stow.py
@@ -2,8 +2,7 @@ import argparse
 import json
 import os
 import sys
-from shutil import copy
-from distutils.dir_util import copy_tree as cptree
+from shutil import copy, copytree
 from pathlib import Path
 
 
@@ -11,7 +10,7 @@ def main():
     if os.getcwd() != os.path.dirname(os.path.abspath(__file__)):
         print("ERROR: must be run within the same directory as script")
         sys.exit(1)
-    with open("stw_dcl.json", "r") as f:
+    with open("stow_dcl.json", "r") as f:
         act = json.load(f)
     parser = argparse.ArgumentParser(prog="stow", description=".file stow")
     parser.add_argument("cfg", help=" ".join([x for x in act]) + " all")
@@ -31,7 +30,7 @@ def stow(c: str, act: dict):
         for f in act[c]["files"]:
             f = Path(f)
             if f.is_dir():
-                cptree(str(f.absolute()), f"{act[c]['dest']}/{f.name}")
+                copytree(str(f.absolute()), f"{act[c]['dest']}/{f.name}")
             else:
                 copy(f.absolute(), f"{act[c]['dest']}/{f.name}")
     elif tp == "text":
@@ -56,7 +55,7 @@ def stow(c: str, act: dict):
         for f in act[c]["files"]:
             f = Path(f)
             if f.is_dir():
-                cptree(str(f.absolute()), os.path.expandvars(f"$HOME/{f}"))
+                copytree(str(f.absolute()), os.path.expandvars(f"$HOME/{f}"))
             else:
                 copy(f.absolute(), os.path.expandvars(f"$HOME/{f}"))
         if not os.path.exists(os.path.expandvars("$HOME/.vim/undodir")):
