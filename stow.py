3e498654a9b86e9d857f09b4063c4bd3a28ead6c
skogsgren <6107181+skogsgren@users.noreply.github.com>
2024-05-14 11:29:13 +0200

    break up stow function to subfunctions

--- stow.py
+++ stow.py
@@ -3,72 +3,82 @@ import json
 import os
 import sys
 from shutil import copy2, copytree, rmtree
+import subprocess
 from pathlib import Path
 
+def stow_files(choice: str, actions: dict) -> None:
+    """ copies files according to specification """
+    for f in actions[choice]["files"]:
+        f = Path(f)
+        srcdir = Path(f)
+        destdir = Path(os.path.expandvars(f"$HOME/{f}"))
+        if f.is_dir():
+            if srcdir.is_dir():
+                if destdir.exists():
+                    rmtree(destdir.absolute())
+                copytree(srcdir, destdir)
+        else:
+            copy2(srcdir, destdir)
+
+def stow_text(choice: str, actions: dict) -> None:
+    """ appends lines to an already existing file """
+    with open(os.path.expandvars(actions[choice]["dest"]), "r") as f:
+        lines = [x.rstrip() for x in f]
+    with open(os.path.expandvars(actions[choice]["dest"]), "a") as f:
+        for l in actions[choice]["cfg"]:
+            if l not in lines:
+                f.write(l + "\n")
+
+def stow_vim(choice: str, actions: dict) -> None:
+    """ installs selected set of vimrc(full/minimal/base) """
+    if actions[choice]["full"]:
+        with open(".vimrc", "r") as f:
+            lines = [x for x in f]
+        with open(os.path.expandvars("$HOME/.vimrc"), "w") as f:
+            for l in lines:
+                if l == "call plug#end()\n":
+                    for x in actions[choice]["extras"]:
+                        f.write(f"source $HOME/.vim/{x}\n")
+                    f.write(l)
+                else:
+                    f.write(l)
+        subprocess.run("./install_coc_extensions.sh")
+    for file in actions[choice]["files"]:
+        srcdir = Path(file)
+        destdir = Path(os.path.expandvars(f"$HOME/{file}"))
+        if srcdir.is_dir():
+            copytree(srcdir, destdir, dirs_exist_ok=True)
+        else:
+            copy2(srcdir, destdir)
+    if not os.path.exists(os.path.expandvars("$HOME/.vim/undodir")):
+        os.makedirs(os.path.expandvars("$HOME/.vim/undodir"))
+
+def stow(choice: str, actions: dict):
+    """ wrapper for stowing dot files with recursive functionality """
+    if choice == "all":
+        for a in actions:
+            stow(choice=a, actions=actions)
+    action_type = actions[choice]["type"]
+    if action_type == "files":
+        stow_files(choice, actions)
+    elif action_type == "text":
+        stow_text(choice, actions)
+    elif action_type == "vim":
+        stow_vim(choice, actions)
 
 def main():
     if os.getcwd() != os.path.dirname(os.path.abspath(__file__)):
         print("ERROR: must be run within the same directory as script")
         sys.exit(1)
     with open("stow_dcl.json", "r") as f:
-        act = json.load(f)
+        actions = json.load(f)
     parser = argparse.ArgumentParser(prog="stow", description=".file stow")
-    parser.add_argument("cfg", help=" ".join([x for x in act]) + " all")
+    parser.add_argument("cfg", help=" ".join(list(actions)) + " all")
     args = parser.parse_args()
-    if args.cfg not in act:
+    if args.cfg not in actions:
         parser.print_help()
     else:
-        stow(args.cfg, act)
-
-
-def stow(c: str, act: dict):
-    if c == "all":
-        for a in act:
-            stow(c=a, act=act)
-    tp = act[c]["type"]
-    if tp == "files":
-        for f in act[c]["files"]:
-            f = Path(f)
-            srcdir = Path(f)
-            destdir = Path(os.path.expandvars(f"$HOME/{f}"))
-            if f.is_dir():
-                if srcdir.is_dir():
-                    if destdir.exists():
-                        rmtree(destdir.absolute())
-                    copytree(srcdir, destdir)
-            else:
-                copy2(srcdir, destdir)
-    elif tp == "text":
-        with open(os.path.expandvars(act[c]["dest"]), "r") as f:
-            lines = [x.rstrip() for x in f]
-        with open(os.path.expandvars(act[c]["dest"]), "a") as f:
-            for l in act[c]["cfg"]:
-                if l not in lines:
-                    f.write(l + "\n")
-    elif tp == "vim":
-        if act[c]["full"]:
-            with open(".vimrc", "r") as f:
-                lines = [x for x in f]
-            with open(os.path.expandvars("$HOME/.vimrc"), "w") as f:
-                for l in lines:
-                    if l == "call plug#end()\n":
-                        for x in act[c]["extras"]:
-                            f.write(f"source $HOME/.vim/{x}\n")
-                        f.write(l)
-                    else:
-                        f.write(l)
-        for f in act[c]["files"]:
-            srcdir = Path(f)
-            destdir = Path(os.path.expandvars(f"$HOME/{f}"))
-            if srcdir.is_dir():
-                if destdir.exists():
-                    rmtree(destdir.absolute())
-                copytree(srcdir, destdir)
-            else:
-                copy2(srcdir, destdir)
-        if not os.path.exists(os.path.expandvars("$HOME/.vim/undodir")):
-            os.makedirs(os.path.expandvars("$HOME/.vim/undodir"))
-
+        stow(args.cfg, actions)
 
 if __name__ == "__main__":
     main()
