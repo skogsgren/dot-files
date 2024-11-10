de81d2e513c0a38621813a815299776ee1679bfb
skogsgren <6107181+skogsgren@users.noreply.github.com>
2024-11-10 16:32:51 +0100

    remove redundant script

--- delete_every_file_except_newest.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd "$1"
-# find the newest file in the folder
-newest_file=$(/bin/ls -t | head -1)
-# delete all files in the folder except the newest one
-find . ! -name "$newest_file" -type f -exec rm {} +
