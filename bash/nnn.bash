d72dc666193ce3683424cedd36840a9544b5846c
skogsgren <6107181+skogsgren@users.noreply.github.com>
2024-11-11 09:36:07 +0100

    more contrast for dir/symlinks

--- bash/nnn.bash
+++ bash/nnn.bash
@@ -15,9 +15,17 @@ nnn ()
 # enables trash-cli
 export NNN_TRASH=1
 
+# increase contrast for directories/symlinks
+export NNN_FCOLORS='c1e21428006027f7c6d6abc4'
+
 # nnn alias
 alias "n"="nnn"
 alias "nn"="nnn"
 
-# small plugin bindings for p:less; j:jq; m:headless mpv
+# small plugin bindings for
+#   p:less
+#   j:jq
+#   m:headless mpv
+#   c:copy filename to clipboard
 export NNN_PLUG='p:-!less -inR "$nnn"*;j:-!cat "$nnn"* | jq . | less -inR*;m:-!mpv --no-video "$nnn"*;v:-!&mpv --no-resume-playback "$nnn"*;c:-!echo "$nnn"* | xclip -i -selection clipboard *;'
+
